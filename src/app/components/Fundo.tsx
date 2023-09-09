5fddbf91446b6441fb16942cc9773dc5b5f12667
MichaelHJacob <michael_h.jacob@hotmail.com>
2023-09-09 02:24:39 -0300

    Update Fundo.tsx

--- src/app/components/Fundo.tsx
+++ src/app/components/Fundo.tsx
@@ -3,8 +3,7 @@ import "../globals.css";
 
 export default function Fundo() {
   return (
-    // <div className="fixed z-[-1] h-screen w-screen  top-0  bg-gradient-to-b dark:from-emerald-950 dark:via-cyan-800 dark:to-gray-950 from-[#766144] via-[#786A49] to-gray-300">
-    <div className="fixed z-[-1] h-screen w-screen  top-0  bg-gradient-to-b dark:from-emerald-950 dark:via-cyan-800 dark:to-gray-950 from-emerald-50 via-cyan-50 to-gray-300">
+    <div className="fixed z-[-1] h-screen w-screen  top-0  bg-gradient-to-b dark:from-emerald-950 dark:via-cyan-800 dark:to-gray-950 from-[#F4F7F7] via-[#c7d3d4] to-[#799ba9]"> 
     
       <span className="skew-x-12 h-screen w-[35%] bg-black/5  absolute left-[-10vw]   "></span>
       <span className="skew-x-12 h-screen w-1/4 bg-black/5  absolute left-[-10vw] "></span>
