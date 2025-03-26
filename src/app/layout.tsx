55ee3462f2d615e46fcda17198bbcee986c72a93
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2025-03-26 17:56:49 -0300

    feat: Adicionar melhor descrição e metatags para indexação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Montserrat } from "next/font/google";
 import Script from "next/script";
+import { WebSite, WithContext } from "schema-dts";
 
 import "./globals.css";
 
@@ -13,8 +14,29 @@ const montserrat = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Michael Henrique Jacob",
-  description: "Portfólio de Michael Henrique Jacob",
+  title: "Michael Henrique Jacob | Desenvolvedor Web Front-End",
+  description:
+    "Desenvolvedor web especializado em React, Next.js e TypeScript. Crio interfaces modernas, responsivas e performáticas. Confira meus projetos e entre em contato!",
+  openGraph: {
+    title: "Michael Henrique Jacob | Desenvolvedor Web Front-End",
+    description:
+      "Desenvolvedor web especializado em React, Next.js e TypeScript. Crio interfaces modernas, responsivas e performáticas. Confira meus projetos e entre em contato!",
+  },
+  verification: {
+    google: "u2e4xEUe9TChs3vZywjhf_vrGmeRXYd56eMZOupAr1Q",
+    me: "michael_h.jacob@outlook.com",
+    other: {
+      ["msvalidate.01"]: "983F53CD12679969F251FD584D892E08",
+    },
+  },
+};
+
+const jsonLd: WithContext<WebSite> = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  name: "Michael H. Jacob",
+  alternateName: ["Michael Henrique Jacob", "MichaelHJacob"],
+  url: "https://michaelhjacob.vercel.app",
 };
 
 export const viewport = {
@@ -30,6 +52,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" className={`${montserrat.variable} h-full w-full`}>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body className={`bg-backgroundPrimary`}>
         {children}
         <Analytics />
