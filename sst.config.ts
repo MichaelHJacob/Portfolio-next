09abd9a25ba329ebcdb21371c706afc9053fbdfb
Michael Henrique Jacob <michael_h.jacob@hotmail.com>
2026-07-03 18:48:43 -0300

    build(sst): configure domain routing and DNS server

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
@@ -8,13 +7,25 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        cloudflare: { package: "@pulumi/cloudflare", version: "6.17.0" },
+        aws: { package: "@pulumi/aws", version: "7.37.0" },
+      },
     };
   },
   async run() {
     const web = new sst.aws.Nextjs("PortfolioWeb", {
       path: "./",
+      domain:
+        $app.stage === "production"
+          ? {
+              name: "mjacob.dev",
+              dns: sst.cloudflare.dns({
+                proxy: true,
+              }),
+            }
+          : undefined,
     });
-
     return {
       siteUrl: web.url,
     };
